7630cb23d0ba95db96f54ef490f12c598aa2852c
cloudykong <cloudy12028@gmail.com>
2020-07-11 19:56:02 +0900

    Fix typo

--- src/xUnitTDD/Assert.java
+++
@@ -1,9 +0,0 @@
-package xUnitTDD;
-
-public class Assert {
-	
-	public static void assertEquals(Object expect, Object actual) {
-		if(!expect.equals(actual))
-			throw new AssertionError("expected: <"+expect+"> but actual <"+actual+">");
-	}
-}
